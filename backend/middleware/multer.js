373256d0952dc4d8be84e72f5c6e530a1f6dd440
pro-jain <154740941+pro-jain@users.noreply.github.com>
2026-06-20 16:49:23 +0530

    Add memory storage option for multer

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,4 +1,6 @@
 import multer from "multer";
+const upload = multer({ storage: multer.memoryStorage() });
+export default upload;
 
 const storage = multer.diskStorage({
     filename:function(req,file,callback){
